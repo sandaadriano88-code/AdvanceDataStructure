15dfe945d81cbaacafd33807c964d74c10901aa6
NkambihBless <nkambihbless@gmail.com>
2026-03-12 12:43:00 +0100

    Added AdvanceDataStructure Files

---
+++ discountCalcutor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int price;
+	printf("Eter the total price");
+	if("total >10000"){
+		printf("You have a 10% discount \n");
+	}
+	else{
+		printf("No discount \n");
+	}
+	printf("discount is %d, &discount");
+	return 0;
+}
+
---
+++ evenNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int num, sum, i;
+	printf("Eter an even number \n");
+	for("int i=0; i<20; i++")
+	{
+		sum=num;
+		num==num%num;
+		num==0;
+	}
+	print("The even numbers are:");
+	
+	return 0;
+}
